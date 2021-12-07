fd454a33b5f3dc709d0edca13dde3bf8ad46978f
sejin-P <chunpark37@gmail.com>
2021-12-07 20:49:58 +0900

    add crud and crud test

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.6.0"
 	kotlin("plugin.spring") version "1.6.0"
 	kotlin("plugin.jpa") version "1.6.0"
+	kotlin("plugin.allopen") version "1.4.32"
 	application
 }
 
@@ -38,6 +39,12 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.Embeddable")
+	annotation("javax.persistence.MappedSuperclass")
 }
 
 application {
