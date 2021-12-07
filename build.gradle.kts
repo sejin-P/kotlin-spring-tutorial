6fc4dacb572d48223b8db19313500e82ab892043
sejin-P <chunpark37@gmail.com>
2021-12-07 21:51:12 +0900

    implement http controller test

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,13 +29,12 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(module = "junit")
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 		exclude(module = "mockito-core")
 	}
 	testImplementation("org.junit.jupiter:junit-jupiter-api")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 	testImplementation("com.ninja-squad:springmockk:3.0.1")
-
 }
 
 tasks.withType<KotlinCompile> {
